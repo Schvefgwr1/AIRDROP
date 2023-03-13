e4081af8f950f07cba95cc714ed559e9e90de08c
Schvefgwr1 <92165301+Schvefgwr1@users.noreply.github.com>
2023-03-13 16:08:14 +0300

    Click Menu

--- src/MainPage/App.js
+++ src/MainPage/App.js
@@ -7,42 +7,49 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React, {Component} from "react";
 import Slider from "react-slick";
+import { useState } from "react";
 
-export default class App extends Component {
-    render() {
-        const settings = {
-            dots: true,
-            dotsClass: "slick-dots slick-thumb",
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            //variableWidth: true,
-            adaptiveHeight: true
-        };
+export default function App() {
 
-        const settings2 = {
-            className: "center",
-            centerMode: true,
-            infinite: true,
-            centerPadding: "60px",
-            slidesToShow: 3,
-            speed: 500,
-            autoplaySpeed: 50
-        };
+    const settings2 = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "60px",
+        slidesToShow: 3,
+        speed: 500,
+        autoplaySpeed: 50
+    };
 
-        return (
-            <div className="App">
-                <div>
-                    <img src={fon} className="Body"/>
-                    <Slider {...settings} className="Slider">
-                        <div>
-                            <h3>Покупка билетов</h3>
-                            <div className="OtkudaKuda">
+    function MenuPart() {
+
+        const [part, setPart] = useState(0);
+
+        const Click = (n) => {
+            console.log("Click good!!!");
+            setPart(n);
+        }
+
+        switch (part) {
+            case 0:
+                return (
+                    <div className="Menu">
+                        <nav className="tabs">
+                            <ul>
+                                <li className="active"><a>Купить билеты</a></li>
+                                <li><a onClick={event => Click(1)}>Статус рейса</a></li>
+                                <li><a onClick={event => Click(2)}>Регистрация на рейс</a></li>
+                                <li><a onClick={event => Click(3)}>Багаж</a></li>
+                                <li><a onClick={event => Click(4)}>Дополнительные услуги</a></li>
+                            </ul>
+                        </nav>
+                        <div className="Form1">
+                            <div className="Form1Part1">
+                                <label htmlFor="date">Укажите место отправки и назначения: </label>
                                 <input className='input1' type='text' placeholder="Откуда"/>
                                 <input className='input1' type='text' placeholder="Куда"/>
                             </div>
-                            <div className="OtkudaKuda">
+                            <div className="Form1Part1">
                                 <label htmlFor="date">Туда: </label>
                                 <input className='input1' type="date" id="date" name="date"/>
                                 <label htmlFor="date">Обратно: </label>
@@ -50,102 +57,163 @@ export default class App extends Component {
                             </div>
                             <div className="Form1Part2">
                                 <input className='input1' type='text' placeholder="Кол-во билетов"/>
-                                <div className="form_radio">
-                                    <input id="radio-1" type="radio" name="radio" value="1" checked/>
-                                        <label htmlFor="radio-1">Эконом</label>
-                                </div>
-                                <div className="form_radio">
-                                    <input id="radio-2" type="radio" name="radio" value="2"/>
-                                        <label htmlFor="radio-2">Бизнес</label>
-                                </div>
+                                <label htmlFor="class">Класс:</label>
+                                <select className='input1'>
+                                    <option value="economy">Эконом</option>
+                                    <option value="business">Бизнес</option>
+                                </select>
                             </div>
-                            <a href="" className="gradient-button">Найти</a>
+                            <div className="Button">
+                                <a href="" className="gradient-button">Найти</a>
+                            </div>
                         </div>
-                        <div>
-                            <h3>Регистрация на рейс</h3>
+                    </div>
+                )
+            case 1:
+                return (
+                    <div className="Menu">
+                        <nav className="tabs">
+                            <ul>
+                                <li><a onClick={event => Click(0)}>Купить билеты</a></li>
+                                <li className="active"><a>Статус рейса</a></li>
+                                <li><a onClick={event => Click(2)}>Регистрация на рейс</a></li>
+                                <li><a onClick={event => Click(3)}>Багаж</a></li>
+                                <li><a onClick={event => Click(4)}>Дополнительные услуги</a></li>
+                            </ul>
+                        </nav>
+                        <div>Про статус рейса</div>
+                    </div>
+                )
+            case 2:
+                return (
+                    <div className="Menu">
+                        <nav className="tabs">
+                            <ul>
+                                <li><a onClick={event => Click(0)}>Купить билеты</a></li>
+                                <li><a onClick={event => Click(1)}>Статус рейса</a></li>
+                                <li className="active"><a>Регистрация на рейс</a></li>
+                                <li><a onClick={event => Click(3)}>Багаж</a></li>
+                                <li><a onClick={event => Click(4)}>Дополнительные услуги</a></li>
+                            </ul>
+                        </nav>
+                        <div>Регистрация на рейс хз что тут будет</div>
+                    </div>
+                )
+            case 3:
+                return (
+                    <div className="Menu">
+                        <nav className="tabs">
+                            <ul>
+                                <li><a onClick={event => Click(0)}>Купить билеты</a></li>
+                                <li><a onClick={event => Click(1)}>Статус рейса</a></li>
+                                <li><a onClick={event => Click(2)}>Регистрация на рейс</a></li>
+                                <li className="active"><a>Багаж</a></li>
+                                <li><a onClick={event => Click(4)}>Дополнительные услуги</a></li>
+                            </ul>
+                        </nav>
+                        <div>Что-то про багаж</div>
+                    </div>
+                )
+            case 4:
+                return (
+                    <div className="Menu">
+                        <nav className="tabs">
+                            <ul>
+                                <li><a onClick={event => Click(0)}>Купить билеты</a></li>
+                                <li><a onClick={event => Click(1)}>Статус рейса</a></li>
+                                <li><a onClick={event => Click(2)}>Регистрация на рейс</a></li>
+                                <li><a onClick={event => Click(3)}>Багаж</a></li>
+                                <li className="active"><a>Дополнительные услуги</a></li>
+                            </ul>
+                        </nav>
+                        <div>мб ваще удалим это</div>
+                    </div>
+                )
+        }
+    }
+
+    return (
+        <div className="App">
+            <div>
+
+                <div className="FirstPart">
+                    {MenuPart()}
+                </div>
+
+                <div className="About">
+                    <h1>О КОМПАНИИ</h1>
+                    <div className="TextAbout">
+                        <div>AirWorld - это авиакомпания, которая предлагает международные и внутренние авиарейсы
+                            для пассажиров, желающих путешествовать по всему миру. Компания имеет сильный флот,
+                            состоящий из современных самолетов различных моделей, которые обеспечивают максимальный
+                            комфорт и безопасность во время полета.
+                            AirWorld предлагает широкий спектр услуг, включая экономичные и бизнес-классы
+                            обслуживания, а также премиум-класс для пассажиров, которые ищут наивысший уровень
+                            комфорта и сервиса. Компания также предоставляет бесплатный Wi-Fi и развлечения во время
+                            полета, включая фильмы, телешоу и музыку.
                         </div>
-                        <div>
-                            <h3>Статус рейса</h3>
+                        <br></br>
+                        <div>AirWorld уделяет большое внимание безопасности и техническому состоянию своих
+                            самолетов, проводя регулярное техническое обслуживание и проверку каждого самолета перед
+                            вылетом. Компания также имеет опытных пилотов и команду профессиональных
+                            бортпроводников, которые готовы оказать помощь и поддержку пассажирам на протяжении
+                            всего полета.
                         </div>
-                        <div>
-                            <h3>Мои бронирования</h3>
+                        <br></br>
+                        <div>AirWorld предлагает удобные варианты бронирования билетов через свой сайт или мобильное
+                            приложение, где пассажиры могут выбрать свой маршрут, удобное время вылета и класс
+                            обслуживания. Компания также предоставляет услуги по оформлению виз и багажа для
+                            пассажиров, чтобы сделать путешествие как можно более комфортным и безопасным.
+                            AirWorld стремится предоставить лучший опыт полета для своих пассажиров и сделать их
+                            путешествие приятным и комфортным.
+                        </div>
+                    </div>
+
+                    <Slider {...settings2} className="Slider2">
+                        <div className="City1">
+                            <h2>Стамбул</h2>
+                            <div>от 9999руб.</div>
+                        </div>
+                        <div className="City2">
+                            <h2>Рим</h2>
+                            <div>от 9999руб.</div>
+                        </div>
+                        <div className="City3">
+                            <h2>Берлин</h2>
+                            <div>от 9999руб.</div>
+                        </div>
+                        <div className="City4">
+                            <h2>Париж</h2>
+                            <div>от 9999руб.</div>
+                        </div>
+                        <div className="City5">
+                            <h2>Аллепо</h2>
+                            <div>от 9999руб.</div>
+                        </div>
+                        <div className="City6">
+                            <h2>Мариуполь</h2>
+                            <div>от 9999руб.</div>
                         </div>
                     </Slider>
-                    <div className="About">
-                        <h1>О КОМПАНИИ</h1>
-                        <div className="TextAbout">
-                            <div>AirWorld - это авиакомпания, которая предлагает международные и внутренние авиарейсы
-                                для пассажиров, желающих путешествовать по всему миру. Компания имеет сильный флот,
-                                состоящий из современных самолетов различных моделей, которые обеспечивают максимальный
-                                комфорт и безопасность во время полета.
-                                AirWorld предлагает широкий спектр услуг, включая экономичные и бизнес-классы
-                                обслуживания, а также премиум-класс для пассажиров, которые ищут наивысший уровень
-                                комфорта и сервиса. Компания также предоставляет бесплатный Wi-Fi и развлечения во время
-                                полета, включая фильмы, телешоу и музыку.
+
+                    <div className="Contacts">
+                        <div className="TextContacts">
+                            <br/>
+                            Поддержка клиентов
+                            <br/>
+                            <img src={tg} alt="" className="Tg"/>
+                            <div className="Number">
+                                +7 (391) 222 - 02 - 22
                             </div>
-                            <br></br>
-                            <div>AirWorld уделяет большое внимание безопасности и техническому состоянию своих
-                                самолетов, проводя регулярное техническое обслуживание и проверку каждого самолета перед
-                                вылетом. Компания также имеет опытных пилотов и команду профессиональных
-                                бортпроводников, которые готовы оказать помощь и поддержку пассажирам на протяжении
-                                всего полета.
-                            </div>
-                            <br></br>
-                            <div>AirWorld предлагает удобные варианты бронирования билетов через свой сайт или мобильное
-                                приложение, где пассажиры могут выбрать свой маршрут, удобное время вылета и класс
-                                обслуживания. Компания также предоставляет услуги по оформлению виз и багажа для
-                                пассажиров, чтобы сделать путешествие как можно более комфортным и безопасным.
-                                AirWorld стремится предоставить лучший опыт полета для своих пассажиров и сделать их
-                                путешествие приятным и комфортным.
+                            <img src={wt} alt="" className="Tg"/>
+                            <div className="Number">
+                                +7 (391) 222 - 02 - 22
                             </div>
                         </div>
 
-                        <Slider {...settings2} className="Slider2">
-                            <div className="City1">
-                                <h2>Стамбул</h2>
-                                <div>от 9999руб.</div>
-                            </div>
-                            <div className="City2">
-                                <h2>Рим</h2>
-                                <div>от 9999руб.</div>
-                            </div>
-                            <div className="City3">
-                                <h2>Берлин</h2>
-                                <div>от 9999руб.</div>
-                            </div>
-                            <div className="City4">
-                                <h2>Париж</h2>
-                                <div>от 9999руб.</div>
-                            </div>
-                            <div className="City5">
-                                <h2>Аллепо</h2>
-                                <div>от 9999руб.</div>
-                            </div>
-                            <div className="City6">
-                                <h2>Мариуполь</h2>
-                                <div>от 9999руб.</div>
-                            </div>
-                        </Slider>
-
-                        <div className="Contacts">
-                            <div className="TextContacts">
-                                <br/>
-                                Поддержка клиентов
-                                <br/>
-                                <img src={tg} alt="" className="Tg"/>
-                                <div className="Number">
-                                    +7 (391) 222 - 02 - 22
-                                </div>
-                                <img src={wt} alt="" className="Tg"/>
-                                <div className="Number">
-                                    +7 (391) 222 - 02 - 22
-                                </div>
-                            </div>
-
-                        </div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 };
